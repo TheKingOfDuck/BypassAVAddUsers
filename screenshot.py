fd0240d9d5839d09e70857f04d6079ddf4404a18
TheKingOfDuck <27958875@qq.com>
2019-02-16 12:34:04 +0800

    截图

--- screenshot.py
+++ screenshot.py
@@ -1,3 +1,7 @@
-from PIL import ImageGrab
-im = ImageGrab.grab()
-im.save('screenshot.png')
+try:
+  from PIL import ImageGrab
+  im = ImageGrab.grab()
+  im.save('screenshot.png')
+  print("success")
+except:
+  print("error")
